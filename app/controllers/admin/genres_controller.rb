f1490dfa90698bc1550901e4992eff580ae3a2f9
EC2 Default User <ec2-user@ip-172-31-28-133.ec2.internal>
2022-05-16 09:27:18 +0000

    [Add]

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,9 +7,13 @@ class Admin::GenresController < ApplicationController
  end
 
  def edit
+   @genre = genre.params(params:[id])
  end
 
  def update
+  @genre = genre.params(params:[id])
+  @genre.update(genre.params)
+  #redirect_to [ジャンル一覧、追加画面へ移動]
  end
 
 end
