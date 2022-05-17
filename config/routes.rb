dca90115ebfc701469f0e292122d9ec3a40a56e0
EC2 Default User <ec2-user@ip-172-31-28-133.ec2.internal>
2022-05-17 12:08:18 +0000

    [Add]ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,35 @@ Rails.application.routes.draw do
   sessions: 'public/sessions'
 }
 
+namespace :admin do
+  get "/homes" => "homes#top"
+  resources :items
+  resources :genres,        only:[:index, :create, :edit, :update]
+  resources :customers,     only:[:index, :show, :edit, :update]
+  resources :orders,        only:[:show, :update]
+  resources :order_details, only:[:update]
+end
+
+scope module: :public do
+  root to:'homes#top'
+  get "/homes" => "homes#top"
+  get "/homes/about" => "homes#about"
+
+  resources :items,         only:[:index, :show]
+
+  resources :customers,     only:[:show, :edit, :update]
+  get "/customers/confirm" => "customers#confirm"
+  patch "/customers/withdrawal" => "customers#withdrawal"
+
+  resources :cart_items,    only:[:index, :update, :destroy, :create]
+  delete "/cart_items/destroy_all" => "cart_items#destroy_all"
+
+  resources :orders,        only:[:new, :create, :index, :show]
+  get "/orders/confirm" => "orders#confirm"
+  get "/orders/conpletion" => "orders#conpletion"
+
+  resources :addresses,     only:[:index, :edit, :create, :update, :destroy]
+end
   #devise_for :admins
   #devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
