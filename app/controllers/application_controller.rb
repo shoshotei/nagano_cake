e6a983a31be7df5afbac057713dd15925ff89772
EC2 Default User <ec2-user@ip-172-31-28-133.ec2.internal>
2022-05-23 11:34:24 +0000

    [Add]新規会員登録画面を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+ protected
+
+ def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_kana_name, :first_kana_name, :postal_code, :address, :telephone_number])
+ end
 end
