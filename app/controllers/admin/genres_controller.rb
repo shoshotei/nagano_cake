c642ec788d2f9ff17dedb156e89369a6388f077c
EC2 Default User <ec2-user@ip-172-31-28-133.ec2.internal>
2022-05-25 11:08:36 +0000

    [update]ジャンル一覧、追加ページの更新

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,19 +1,23 @@
 class Admin::GenresController < ApplicationController
  def index
-    @genres = genre.all
+    @genres = Genre.all
+    @genre = Genre.new
  end
 
  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
  end
 
  def edit
-   @genre = genre.params(params:[id])
+    @genre = genre.params(params:[id])
  end
 
  def update
-  @genre = genre.params(params:[id])
-  @genre.update(genre.params)
-  #redirect_to [ジャンル一覧、追加画面へ移動]
+   @genre = genre.params(params:[id])
+   @genre.update(genre.params)
+   redirect_to admin_genres_path
  end
 
 end
