599993eb5135a708bea79dce4f4ae3a718aeff1f
EC2 Default User <ec2-user@ip-172-31-28-133.ec2.internal>
2022-06-07 12:29:03 +0000

    [Add]注文情報確認画面の製作

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,16 @@
 class Public::OrdersController < ApplicationController
-  
+
   def new
   end
 
   def confirm
+    @cart_items = current_customer.cart_items
+    @order = Order.new(order_params)
+    @order.shipping_cost = 800
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
   end
 
   def conpletion
@@ -16,6 +23,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 
 end
