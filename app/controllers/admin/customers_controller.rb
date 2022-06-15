682b6ccfc8e9de2e62df1b79b6f94332bc826b23
EC2 Default User <ec2-user@ip-172-31-28-133.ec2.internal>
2022-06-15 11:43:57 +0000

    [Add]管理者側の詳細画面、編集画面の編集と修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,23 +2,27 @@ class Admin::CustomersController < ApplicationController
 
   def index
     @customers = Customer.page(params[:page])
-    @customer = Customer.find(params[:id])
+
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
-
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
   end
 
   private
 
   def customer_params
-    params.require(:customer).permit(:id, :name, :email, :is_active)
+    params.require(:customer).permit(:id, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_active)
   end
 
 end
