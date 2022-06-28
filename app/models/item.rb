53bb1f4468332a42e47f0573407ab6b63faece15
EC2 Default User <ec2-user@ip-172-31-28-133.ec2.internal>
2022-06-28 12:27:15 +0000

    [Addレイアウトの調整]

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,12 +4,12 @@ class Item < ApplicationRecord
   has_many :cart_items
   has_many :order_details
 
-   def get_image
+   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [width, height]).processed
    end
 
    def  with_tax_price
