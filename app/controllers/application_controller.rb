aa621d7f3a083e5f45ed9ff3b63fe3833f467201
EC2 Default User <ec2-user@ip-172-31-28-133.ec2.internal>
2022-07-03 05:34:01 +0000

    [Add]管理者ログイン画面の編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,24 @@ class ApplicationController < ActionController::Base
 
  protected
 
+ def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+        admin_homes_path
+    else
+        root_path
+    end
+ end
+
+ def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :customer
+        root_path
+    elsif resource_or_scope == :admin
+        new_admin_session_path
+    else
+        root_path
+    end
+ end
+
  def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
  end
